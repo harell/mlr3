e9b7bc32d1b3ee346bbdd2a1f77785666a584429
harell <harel@loostiger.com>
2020-09-01 19:17:53 +1200

    update system files

--- tests/testthat/test-zzz.R
+++ tests/testthat/test-zzz.R
@@ -6,9 +6,3 @@ test_that("Global options are defined", {
     expect_true(identical(is.null(.Options$path_inst), FALSE), info = err_msg)
 })
 
-# hook for test coverage --------------------------------------------------
-if(Sys.getenv("R_COVR") == "true")
-    test_that("component-tests", {
-        path <- file.path(dirname(getwd()), "component-tests")
-        testthat::test_dir(path)
-    })
