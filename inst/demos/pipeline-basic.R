ba6ff000c517a3c7a4df8dd0fdc959f12fe01a9d
harell <harel@loostiger.com>
2020-09-13 20:59:33 +1200

    new script

---
+++ inst/demos/pipeline-basic.R
@@ -0,0 +1,4 @@
+################################################################################
+## mlr3pipelines Tutorial - German Credit
+## <https://mlr3gallery.mlr-org.com/posts/2020-03-11-mlr3pipelines-tutorial-german-credit/>
+################################################################################
