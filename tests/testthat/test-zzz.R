3aec106d6349e6002e030488df78f3b380a34029
harell <harel@loostiger.com>
2020-09-15 20:02:03 +1200

    remove packages

--- tests/testthat/test-zzz.R
+++
@@ -1,8 +0,0 @@
-context("unit test for package hooks")
-
-# onAttach ----------------------------------------------------------------
-test_that("Global options are defined", {
-    err_msg <- "onAttach did not load config-r.yml"
-    expect_true(identical(is.null(.Options$path_inst), FALSE), info = err_msg)
-})
-
